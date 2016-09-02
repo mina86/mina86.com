75d95d7ca0eaed03f8b1fddb0895afb46720a42e
Michal Nazarewicz <mina86@mina86.com>
2016-09-02 21:21:53 +0200

    Fix sidebar if window is high enough to fit it all

--- src/js.js
+++ src/js.js
@@ -1,24 +1,56 @@
 (function(D, W) {
-	var addScript = function(src) {
-		node = D.createElement('script');
-		node.src = 'https://' + src;
-		D.head.appendChild(node);
+	/* Third party scripts */
+	var a = function(src) {
+		b = D.createElement('script');
+		b.src = 'https://' + src;
+		(D.head || D.body).appendChild(b);
 	},
-	    node;
+	    b = D.getElementById,
+	    docEl = D.documentElement,
+	    sidebar = b.call(D, 's'),
+	    header = b.call(D, 'h'),
+	    footer = b.call(D, 'f');
 
-	/* Spot.IM Comments */
 	W['SPOTIM'] = {spotId: 'sp_yH01UA8B'};
-	addScript('www.spot.im/launcher/bundle.js');
+	a('www.spot.im/launcher/bundle.js');
 
-	/* Google Analytics */
 	W['_gaq'] = [['_setAccount', 'UA-240278-1'], ['_trackPageview']];
-	addScript('www.google-analytics.com/ga.js');
+	a('www.google-analytics.com/ga.js');
 
-	/* Social */
 	W['___gcfg'] = {lang: 'en-GB'};
-	addScript('apis.google.com/js/plusone.js');
-	addScript('platform.twitter.com/widgets.js');
+	a('apis.google.com/js/plusone.js');
+	a('platform.twitter.com/widgets.js');
 
-	/* Google Custom Search */
-	addScript('cse.google.com/cse.js?cx=005697715059674104273:zluc68s5jow');
+	a('cse.google.com/cse.js?cx=005697715059674104273:zluc68s5jow');
+
+	/* Sidebar pinning */
+	sidebar && header && footer && (W.onscroll = W.onresize = function() {
+		a = a && (W.setTimeout(function() {
+			a = sidebar.getBoundingClientRect;
+			var r = a.call(sidebar),
+			    cls = sidebar.className.replace(/\s*\bv\b/, b = ''),
+			    top = r.bottom - r.top;
+
+			/* For sidebar to be pinned the following must hold:
+			   - header must be off screen, i.e. it’s bottom must be
+			     non-positive in respect to view port and
+			   - sidebar’s height must be no greater than available
+			     client height. */
+			if (a.call(header).bottom > 0 ||
+			    top > docEl.clientHeight) {
+				top = b;
+			} else {
+				cls += ' v';
+				top = a.call(footer).top - top;
+				top = top < 0 ? top + 'px' : '0';
+				b = (W.pageXOffset || docEl.scrollLeft || 0) -
+					a.call(sidebar.parentElement).right +
+					r.right + r.left + 'px';
+			}
+
+			sidebar.className = cls;
+			sidebar.style.left = b;
+			sidebar.style.top = top;
+		}, 0), 0);
+	})();
 })(document, window);
