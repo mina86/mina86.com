666f7bd043d1af996ae4aeb1ae4fce6f30602e26
Michal Nazarewicz <mina86@mina86.com>
2019-02-04 18:27:59 +0000

    Fix sidebar position updats being delyed and simplify JS code
    Remove setTimeout from the onscroll and onresize handlers, name some
    of the variables better and use Array.prototype.map to shorten the
    script.
    
    With the setTimeout call, if user continuously scrols the page (i.e.
    lets mouse’s scroll wheel roll), Chrome would delay update of the
    position till the scrolling stops.  This sounds good for limiting
    computations but it means that sidebar would overshoot or stay out
    of the viewing area in certain situations.

--- src/js.js
+++ src/js.js
@@ -1,55 +1,50 @@
 (function(D, W) {
-	/* Third party scripts */
-	var a = function(src) {
-		b = D.createElement('script');
-		b.src = 'https://' + src;
-		(D.head || D.body).appendChild(b);
-	},
-	    b = D.getElementById,
+	var tmp = D.getElementById,
 	    docEl = D.documentElement,
-	    sidebar = b.call(D, 's'),
-	    header = b.call(D, 'h'),
-	    footer = b.call(D, 'f');
+	    sidebar = tmp.call(D, 's'),
+	    header = tmp.call(D, 'h'),
+	    footer = tmp.call(D, 'f');
 
-	a('cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_CHTML');
-
-	a('platform.twitter.com/widgets.js');
-
-	a('launcher.spot.im/spot/sp_yH01UA8B');
-
+	/* Third party scripts */
 	W['_gaq'] = [['_setAccount', 'UA-240278-1'], ['_trackPageview']];
-	a('www.google-analytics.com/ga.js');
-
-	a('cse.google.com/cse.js?cx=005697715059674104273:zluc68s5jow');
+	[
+		'cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_CHTML',
+		'platform.twitter.com/widgets.js',
+		'launcher.spot.im/spot/sp_yH01UA8B',
+		'www.google-analytics.com/ga.js',
+		'cse.google.com/cse.js?cx=005697715059674104273:zluc68s5jow'
+	].map(function(src) {
+		tmp = D.createElement('script');
+		tmp.src = 'https://' + src;
+		(D.head || D.body).appendChild(tmp);
+	});
 
 	/* Sidebar pinning */
 	sidebar && header && footer && (W.onscroll = W.onresize = function() {
-		a = a && (W.setTimeout(function() {
-			a = sidebar.getBoundingClientRect;
-			var r = a.call(sidebar),
-			    cls = sidebar.className.replace(/\s*\bv\b/, b = ''),
-			    top = r.bottom - r.top;
+		var getRect = sidebar.getBoundingClientRect,
+		    sidebarRect = getRect.call(sidebar),
+		    cls = sidebar.className.replace(/\s*\bv\b/, tmp = ''),
+		    top = sidebarRect.bottom - sidebarRect.top;
 
-			/* For sidebar to be pinned the following must hold:
-			   - header must be off screen, i.e. it’s bottom must be
-			     non-positive in respect to view port and
-			   - sidebar’s height must be no greater than available
-			     client height. */
-			if (a.call(header).bottom > 0 ||
-			    top > docEl.clientHeight) {
-				top = b;
-			} else {
-				cls += ' v';
-				top = a.call(footer).top - top;
-				top = top < 0 ? top + 'px' : '0';
-				b = (W.pageXOffset || docEl.scrollLeft || 0) -
-					a.call(sidebar.parentElement).right +
-					r.right + r.left + 'px';
-			}
+		/* For sidebar to be pinned the following must hold:
+		   - header must be off screen, i.e. it’s bottom must be
+		     non-positive in respect to view port and
+		   - sidebar’s height must be no greater than available client
+		     height. */
+		if (getRect.call(header).bottom > 0 ||
+		    top > docEl.clientHeight) {
+			top = tmp;
+		} else {
+			cls += ' v';
+			top = getRect.call(footer).top - top;
+			top = top < 0 ? top + 'px' : '0';
+			tmp = (W.pageXOffset || docEl.scrollLeft || 0) -
+				getRect.call(sidebar.parentElement).right +
+				sidebarRect.right + sidebarRect.left + 'px';
+		}
 
-			sidebar.className = cls;
-			sidebar.style.left = b;
-			sidebar.style.top = top;
-		}, 0), 0);
+		sidebar.className = cls;
+		sidebar.style.left = tmp;
+		sidebar.style.top = top;
 	})();
 })(document, window);
